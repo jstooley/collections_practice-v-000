fbd72eecb6728cd0130a81a0963dab2722d43998
Justin Tooley <jstooley823@gmail.com>
2017-09-06 09:39:40 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
+def sort_array_asc(array)
+  array.sort!
+end
 
+def sort_array_desc(array)
+  sort_array_asc(array).reverse!
+end
+
+def sort_array_char_count(array)
+  array.sort do |wordone,wordtwo|
+    wordone.length <=> wordtwo.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  array
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker(array)
+  x = 0
+  new_array = []
+  array.each do |name|
+    temp_array = []
+    i = 0
+    name.each_char do|c|
+      temp_array[i] = c
+      i += 1
+    end
+    temp_array[2] = "$"
+    new_array[x] = temp_array.join
+    x += 1
+  end
+  new_array
+end
+
+def fins_a(array)
+  array.start_with?("a")
+end
